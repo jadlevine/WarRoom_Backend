f0090cfc4bc861346e13800835b4ab9ceb50fbb5
Josh Levine <levine.joshuaadam@gmail.com>
2023-02-10 12:13:24 -0500

    initial setup

---
+++ src/main/java/sh/joshlevine/WarRoom_Backend/WarRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package sh.joshlevine.WarRoom_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarRoomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarRoomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/joshlevine/WarRoom_Backend/WarRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.joshlevine.WarRoom_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarRoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
